0d10b90295f61d1697abc5d4e9644a86d138bb84
bildersee <adambildersee@gmail.com>
2020-10-09 14:56:22 -0700

    Modify Icons.js page.

--- js/src/page/internal/Icons.js
+++ js/src/page/internal/Icons.js
@@ -1,42 +1,47 @@
 import './Icons.scss';
 import Card from 'components/chrome/Card';
+import LabelValue from 'components/chrome/LabelValue';
 import React, {useEffect, useState} from 'react';
 import PlayerHelper from 'players/PlayerHelper';
 
 
-const NAMES = [
-];
-
-const MAP = {
+const imp = {
+  'games/': path => import(`games/${path}`),
 };
+
+const dynamicImport = async (path, callback) => {
+  // Need to have path start with literal for webpack static analysis.
+  let match = /(\w+\/)(.*)/.exec(path);
+  imp[match[1]](match[2]).then(module => callback(module.default), err => null);
+};
+
+const dynImp = async (card, label, path, setFn) => {
+  dynamicImport(path, url => {
+    setFn(oldVal => {
+      if (oldVal[card] && oldVal[card].find(i => i.path === path)) {
+        return oldVal;
+      }
+      let newVal = Object.assign({}, oldVal);
+      newVal[card] = newVal[card] || [];
+      newVal[card].push({path, label, url});
+      return newVal;
+    });
+  });
+};
+
 
 const Icons = props => {
   const [importedIcons, setImportedIcons] = useState({});
 
   useEffect(() => {
-    // Dandelions images.
-    let imports = new Array(12).fill().map((_, i) => import(`games/dandelions/instructions/media/image${i+1}.png`));
-    Promise.all(imports).then(
-        imgs => {
-          let iconMap = {};
-          imgs.forEach(
-              (result, i) =>
-                  iconMap[`dandelions-instructions-${i+1}`] = result.default);
-          setImportedIcons(importedIcons => Object.assign({}, importedIcons, iconMap));
-        });
-  }, []);
-
-
-  const renderIcon = name => {
-    return (
-      <div className='container p-col-2' key={name}>
-        <Card
-            img={MAP[name]}
-            text={name}
-            />
-      </div>
+    ['dandelions', 'prophecies', 'sequencium'].forEach(gameName =>
+      new Array(12).fill().map((_, i) => dynImp(
+          `${gameName} Instructions`,
+          `${gameName} - image${i + 1}`,
+          `games/${gameName}/instructions/media/image${i + 1}.png`,
+          setImportedIcons))
     );
-  };
+  }, []);
 
   const renderPlayerIcons = () => {
     return (
@@ -51,22 +56,37 @@ const Icons = props => {
     );
   };
 
-  const renderImportedIcons = () => {
-    console.log(importedIcons);
+  const renderImportedIconsCard = cardName => {
+    let cardData = importedIcons[cardName].sort((a, b) => a.label > b.label);
     return (
-      <Card childrenPadded header='Imports'>
-        {Object.keys(importedIcons).map(key =>
-            <img key={key} alt={key} src={importedIcons[key]} style={{'maxWidth': '200px', 'border': '1px solid gray'}} />)}
+      <Card childrenPadded header={cardName} key={cardName}>
+        {cardData.map(cd => (
+          <LabelValue
+              key={cd.label}
+              label={<img
+                  alt={cd.label} src={cd.url}
+                  style={{'border': '1px solid gray'}} />}
+              value={cd.label}
+              />
+        ))}
       </Card>
     );
-  }
+  };
+
+  const renderImportedIcons = () => {
+    let cards = Object.keys(importedIcons).sort().map(renderImportedIconsCard);
+    return <div>{cards}</div>;
+  };
 
   return (
     <div className='section'>
       <div className='p-grid'>
-        {renderImportedIcons()}
-        {renderPlayerIcons()}
-        {NAMES.map(renderIcon)}
+        <div className='p-col-12'>
+          {renderImportedIcons()}
+        </div>
+        <div className='p-col-12'>
+          {renderPlayerIcons()}
+        </div>
       </div>
     </div>
   );
